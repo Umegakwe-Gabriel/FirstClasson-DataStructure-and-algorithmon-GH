3336ad413b730d8d8925ed1cb7d9924f13b20b45
Kingsman <kingsmandralph@gmail.com>
2023-04-27 20:33:29 +0000

    what's wrong

--- linklist5.js
+++ linklist5.js
@@ -1,42 +1,40 @@
-const { log } = require("console");
-
 class Node {
-    constructor(val){
+    constructor (val){
         this.val = val;
         this.next = null;
     }
 }
 
-class Linklist {
+class Linklist5 {
     constructor(){
         this.head = null;
         this.tail = null;
         this.length = 0;
     }
 
-    insert (){
-        let node = new Node (val);
+    insert(val){
+        let node = new Node (val)
         if (!this.head) {
             this.head = node;
-            this.tail = head;
+            this.tail = this.head;
         } else {
             this.tail.next = node;
-            this.tail = node;            
+            this.tail = node;
         }
         this.length++;
         return this;
     }
 
-    remove(){
-        if (!this.head){
-            return undefined;
-        }else {
+    remove (){
+        if (!this.head) {
+            return undefined
+        } else {
             let current = this.tail;
             let newTail = current;
 
-            while (current.next) {
-               this.tail = newTail;
-               this.tail.next = null; 
+            while (current.next){
+                this.tail = newTail;
+                this.tail.next = null;
             }
 
             this.length--;
@@ -45,13 +43,14 @@ class Linklist {
     }
 }
 
-let linkedList = new Linklist();
+let linkedList = new Linklist5();
 console.log(linkedList.insert(39));
-console.log(linkedList.insert(34));
+console.log(linkedList.insert(39));
 
-console.log(linkedList.insert([34, 3, 4, 5]));
-console.log(linkedList.insert(54));
-console.log(linkedList.insert({name: "Micheal"}));
+console.log(linkedList.insert(39));
+console.log(linkedList.insert(39));
+console.log(linkedList.insert(39));
+console.log(linkedList.insert(39));
 console.log("");
 console.log(linkedList.length);
 linkedList.remove();
